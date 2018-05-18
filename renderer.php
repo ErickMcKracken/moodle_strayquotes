b46685f468ffc500f0d24a0f7343be400659c209
Eric Frenette <frenette.eric@uqam.ca>
2018-05-18 15:41:22 -0400

    Ajout feuille de style. Ajout d'un renderer. Modificaltion de la classe principale pour accomoder l'utilisation du renderer.  Formatage de la sortie HTML.

--- renderer.php
+++ renderer.php
@@ -7,8 +7,25 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
+/*******************************************************/
+/*          Display the quote in the block             */
+/*******************************************************/
+
 class block_strayquotes_renderer extends plugin_renderer_base{
-    public function get_quote(){
-        
+    public function display_quote($quote, $author_name, $author_picture, $source){
+         
+        $content = html_writer::start_tag('div', array('class' => 'block_strayquotes_quote'));
+        $content = html_writer::start_tag('div', array('class' => 'block_strayquotes_quoteDetails'));
+        $content .= html_writer::empty_tag('img', array('src' => $author_picture, 'alt' => $author_name, 'class' => 'block_strayquotes_authorpix'));
+        $content .= html_writer::start_span('block_strayquotes_quoteText') . $quote . html_writer::end_span();
+       // $content .= html_writer::html($quote);
+        $content .= html_writer::label($quote, array('class' => 'block_strayquotes_quoteText'))  ;
+        $content .= html_writer:: tag('br');
+        $content .= html_writer::start_span('quoteauthor') . $author_name . html_writer::end_span();
+        $content .= html_writer:: tag('br');
+        $content .= html_writer::start_span('quoteauthor') . $source . html_writer::end_span();
+        $content .= html_writer::end_tag('div');
+        $content .= html_writer::end_tag('div');
+        return $content;
     }
 }
